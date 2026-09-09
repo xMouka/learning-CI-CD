fd7fe4c2079ec696396b1dc2c92497c5c1719f93
xMouka <faysalgamer2016@gmail.com>
2026-09-09 18:48:52 +0100

    feat: add CI pipeline

--- test.js
+++ test.js
@@ -1,3 +1,3 @@
 console.log('Starting the tests ...');
-setTimeout(console.log('wait 3 seconds ...'), 3000);
+setTimeout(() => console.log('wait 3 seconds ...'), 3000);
 console.log('Tests completed successfully!');
